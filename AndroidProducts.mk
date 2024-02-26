ed57610977a33ccecbf21c79e99c3e7768cc3628
bzoomb <bzoomb1@gmail.com>
2024-02-26 21:47:51 +0700

    trying to build with this one
    reference: https://github.com/IMYdev/twrp_device_infinix_sword

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_Infinix-X670.mk
+    $(LOCAL_DIR)/twrp_X670.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_Infinix-X670-user \
-    omni_Infinix-X670-userdebug \
-    omni_Infinix-X670-eng
+    twrp_X670-eng
--- twrp_X670.mk
+++ twrp_X670.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from Infinix-X670 device
-$(call inherit-product, device/infinix/Infinix-X670/device.mk)
+# Inherit from X670 device
+$(call inherit-product, device/infinix/X670/device.mk)
 
-PRODUCT_DEVICE := Infinix-X670
-PRODUCT_NAME := omni_Infinix-X670
+PRODUCT_DEVICE := X670
+PRODUCT_NAME := twrp_X670
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X670
 PRODUCT_MANUFACTURER := infinix
@@ -26,4 +26,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-infinix
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vnd_x670_h814-user 12 SP1A.210812.016 452239 release-keys"
 
-BUILD_FINGERPRINT := Infinix/X670-GL/Infinix-X670:12/SP1A.210812.016/231013V390:user/release-keys
+BUILD_FINGERPRINT := Infinix/X670-GL/X670:12/SP1A.210812.016/231013V390:user/release-keys
